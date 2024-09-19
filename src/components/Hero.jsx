4a4ba05783613eb9702f64f887717b058f281488
kemelite1 <osikemelite42@gmail.com>
2024-09-19 14:42:00 +0100

    Add Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section className="h-screen bg-green-700 bg-cover flex items-center">
+        <div className="container mx-auto px-6 text-center">
+            <h1 className="text-5xl text-white font-bold">Transactions without middlemen</h1>
+            <p className="text-white text-lg mt-4">Buy and sell your fresh fruits and vegetables seamlessly</p>
+            <button className="bg-green-500 text-white font-bold px-6 py-3 mt-6 rounded-lg">Get Started</button>
+        </div>
+    </section>
+  );
+}
+
+export default Hero
