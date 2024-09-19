5d34ac30ea0946e5ec3c7ab2b19fd11904b4dea7
kemelite1 <osikemelite42@gmail.com>
2024-09-19 16:52:07 +0100

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
+import Logo from '../assets/images/logo.jpeg'
 const Navbar = () => {
   return (
     <nav className="w-full bg-white shadow-lg fixed top-0">
       <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-        <div className="text-2xl text-gray-500 font-bold">Konectar</div>
-        <div className="space-x-4">
+        <div className="flex items-center">
+          <img src={Logo} alt="Konectar Logo" className="w-20 h-20" />
+        </div>
+        {/* <div className="text-2xl text-gray-500 font-bold">Konectar</div> */}
+        <div className=" hidden md:flex space-x-4">
           <a href="#" className="text-gray-600 hover:text-gray-900">
             Home
           </a>
@@ -16,7 +20,9 @@ const Navbar = () => {
           <a href="#reviews" className="text-gray-600 hover:text-gray-900">
             Reviews
           </a>
-          <button className="px-4 py-2 bg-green-500 text-white roundedl">Sign Up</button>
+          <button className="px-4 py-2 bg-green-500 text-white roundedl">
+            Sign Up
+          </button>
         </div>
       </div>
     </nav>
